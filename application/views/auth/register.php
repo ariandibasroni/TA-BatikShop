df7eb6b54b63a4570885defcb86d31cae7d3ecc3
Heru setiawan <116283513+Herusetiawan19313035@users.noreply.github.com>
2023-01-03 04:21:14 -0800

    Update register.php

--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -11,4 +11,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta name="description" content="Colorlib Templates">
     <meta name="author" content="Colorlib">
     <meta name="keywords" content="Colorlib Templates">
+    
+    <!-- Title Page-->
+    <title>Daftar dan Buat Akun | <?php echo get_store_name(); ?></title>
+
+    <!-- Icons font CSS-->
+    <link href="<?php echo get_theme_uri('custom/auth/register/vendor/mdi-font/css/material-design-iconic-font.min.css'); ?>" rel="stylesheet" media="all">
+    <link href="<?php echo get_theme_uri('custom/auth/register/vendor/font-awesome-4.7/css/font-awesome.min.css'); ?>" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+
+    <!-- Vendor CSS-->
+    <link href="<?php echo get_theme_uri('custom/auth/register/vendor/select2/select2.min.css'); ?>" rel="stylesheet" media="all">
+    <link href="<?php echo get_theme_uri('custom/auth/register/vendor/datepicker/daterangepicker.css'); ?>" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="<?php echo get_theme_uri('custom/auth/register/css/main.css'); ?>" rel="stylesheet" media="all">
+
+    <style>
+        .input--style-2:hover {
+            border-bottom: 1px solid #fa4251;
+            color: #4DAE3C
+        }
+    </style>
 </head>
